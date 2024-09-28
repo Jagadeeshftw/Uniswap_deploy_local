528f946d80de0aa98197211e24ad927f05325468
Jagadeeshftw <jagadeesh26062002@gmail.com>
2024-09-28 10:28:22 +0530

    Remove unused files

--- script/Imports-factory.s.sol
+++
@@ -1,4 +0,0 @@
-// SPDX-License-Identifier: SEE LICENSE IN LICENSE
-pragma solidity >=0.5.0;
-
-import {UniswapV2Factory} from "@uniswap/v2-core/contracts/UniswapV2Factory.sol";
--- test/Tether.t.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.20;
-
-import {Test, console} from "forge-std/Test.sol";
-import {Tether} from "../src/Tether.sol";
-
-contract TestTether is Test {
-   
-}
